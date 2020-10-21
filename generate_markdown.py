cb51ac9d45df2ede996e1d03549cda0f346dd48c
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2020-10-21 16:23:13 +0200

    filter on CODEOWNERS

--- generate_markdown.py
+++ generate_markdown.py
@@ -1,25 +1,41 @@
 #!/usr/bin/env python3 
 
 import json
+import sys
 import textwrap
-from pathlib import Path
 
-doc_body = textwrap.dedent('''\
-    # Helikopteroversikt
 
-    | Reponavn | Beskrivelse |
-    | :------: | :---------- |
+def is_owned_by_aura(repo):
+    return any(
+        owner in repo.get('CODEOWNERS', '')
+        for owner
+        in ('navikt/aura', 'nais/aura')
+    )
+
+
+def make_table(data, *, predicate=is_owned_by_aura):
+    table = textwrap.dedent('''\
+        | Reponavn | Beskrivelse |
+        | :------: | :---------- |
     ''')
-
-if __name__ == '__main__':
-    data = json.loads(Path('indata.json').read_text())
     for org_name, repos in data.items():
         for repo_name, repo in repos.items():
+            if not predicate(repo): continue
             desc = repo['gh_data']['description']
-            doc_body += textwrap.dedent(f"""\
+            table += textwrap.dedent(f"""\
                 | [{org_name}/{repo_name}]({repo['gh_data']['html_url']}) \
-                | {desc if desc else '**INGEN BESKRIVELSE**'} |
+                | {desc if desc else '**Mangler beskrivelse!**'} |
             """)
+    return table
 
 
+if __name__ == '__main__':
+    data = json.load(sys.stdin)
+    doc_body = textwrap.dedent('''\
+    # Helikopteroversikt
+
+    Dette er en oversikt over repositories med enten `navikt/aura` eller `nais/aura` i sin CODEOWNERS.
+
+    ''')
+    doc_body += make_table(data, predicate=is_owned_by_aura)
     print(doc_body)
