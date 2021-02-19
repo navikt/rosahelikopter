9136489c912ed25ca18a7baddfd53e14c252c0ff
Christian Chavez <christian.chavez@nav.no>
2021-02-19 18:07:22 +0100

    Refactor(cli): Improve CLI help text/documentation.

--- rosahelikopter/cli.py
+++ rosahelikopter/cli.py
@@ -3,92 +3,113 @@
 Sub-module defining the program's interaction with the user via the commandline.
 """
 
+# Python standard library imports
 import json
 import textwrap
 
+# Non-standard library python package imports
 import click
 
+# Imports of module(s) internal to this project/package
 from rosahelikopter.main import main
+
+DEFAULT_TEAM_NAMES=(
+    'aura',
+    'nais',
+    'naisdevice',
+)
 
 
 @click.command(
+    options_metavar='<options>',
     context_settings=dict(
         help_option_names=['-h', '--help'],
         max_content_width=120,
+    ),
+    help=textwrap.dedent(
+        f"""List all (un-archived) repositories in <GITHUB ORGS> for which the <GITHUB TEAMS> are ADMINs of.
+        Output is a Markdown file with one repository per row in a table.
+        The table has the columns: `Name (as GH URL)` and `Description (GH Repo description)`.
+
+        <GITHUB TEAMS> is one or more Github team names (strings).
+        <GITHUB TEAMS> defaults to the Github team names {DEFAULT_TEAM_NAMES} if unspecified."""
     )
 )
 @click.option(
     '-o', '--github-org', 'organizations',
-    default=('navikt', 'nais'), multiple=True, type=str,
+    default=('navikt', 'nais'), multiple=True,
+    type=str, metavar='<GITHUB ORGS>', show_default=True,
     help=textwrap.dedent(
-        """\
-        One or more Github organization names whose repositories this tool will iterate through.
-        Can be specified one per `-o/--github-org` flag.
-        """
+        """One or more Github organization names whose repositories this tool will iterate through.
+        Multiple can be specified as one per `-o/--github-org` flag."""
     ),
 
 )
 @click.argument(
-    'TEAMS', nargs=-1, type=str,
+    'TEAMS', nargs=-1, type=str, metavar='<GITHUB TEAMS>',
 )
 @click.option(
-    '--separate-output-per-team', 'separate_team_output', is_flag=True,
-    help='Flag to tell the program to separate outputs into files separated by folders per organization.',
+    '-T', '--separate-output-per-team', 'make_team_files', is_flag=True,
+    help=textwrap.dedent(
+        """Flag to change output of program from stdout to one file per team named '<team name>.md'.
+        Can be used in conjunction with `--separate-output-per-org` flag.
+
+        Intended for use when wanting to know which team has a relation to which repositories,
+        """
+    ),
 )
 @click.option(
-    '--separate-output-per-org', 'separate_org_output',
+    '-O', '--separate-output-per-org', 'make_org_folders',
     is_flag=True, default=False,
-    help='Flag to tell the program to separate outputs into one file per team.'
+    help=textwrap.dedent(
+        """Flag to output of program from stdout to `<org name>/overview.md` files.
+        Can be used in conjunction with `--seperate-output-per-team` flag."""
+    ),
+)
+@click.option(
+    '-a', '--github-auth-token',
+    type=str, envvar='GITHUB_USER_TOKEN', required=True,
+    help=textwrap.dedent(
+        """User token (PAT) with which to authenticate against Githubs GraphQL API.
+        Defaults to environment variable "GITHUB_USER_TOKEN" if present."""
+    ),
 )
 @click.option(
     '--verbose', '-v', 'verbosity',
     type=int, count=True,
     help=textwrap.dedent(
-        """\
-        Verbosity level.
-
-        \b
+        """Verbosity level.
         Causes program to increase debug output during its execution.
         Multiple `-v`s increase verbosity.
 
-        "Final level of verbosity" (V) is calculated as `V = sum(verbosity) - sum(silent)`
-        """
+        Final level of verbosity (V) is calculated as `V = sum(verbose) - sum(silent)`"""
     ),
 )
 @click.option(
     '--silent', '-s', 'silence',
     type=int, count=True,
     help=textwrap.dedent(
-        """\
-        Quiet/silence level.
-
-        \b
-        Causes program to silence debug output during its execution.
+        """Quiet/silence level.
+        Causes program to decrease debug output during its execution.
         Multiple `-s`s decrease verbosity.
 
-        "Final level of verbosity" (V) is calculated as `V = sum(verbosity) - sum(silent)`
-        """
+        Final level of verbosity (V) is calculated as `V = sum(verbose) - sum(silent)`"""
     ),
 )
-@click.pass_context
+# @click.pass_context
 def cli(
-    ctx,
+    # 'ctx' is required for click CliRunner integration tests
+    #   when checking exit_code, and for when to exit program early.
+    # ctx,
     organizations: list[str],
     teams: list[str],
-    separate_org_output: bool,
-    separate_team_output: bool,
+    make_org_folders: bool,
+    make_team_files: bool,
+    github_auth_token: str,
     verbosity: int,
     silence: int,
 ):
-    """List all repositories in GITHUB-ORG for  which the GITHUB-TEAMS have ADMIN role.
-
-    One or more Github team names that must have some access rights to the repositories in the specified orgs.
-    This argument parameter is a variadic one and doesn't need any `-X/--XY` flagging.
-
-    Defaults to Github team names 'nais' and 'aura' if unspecified."""
-
-    local_vars = {**locals()}
-
+    local_vars = {name: value for name, value in locals().items() if not name.startswith('_')}
     # Set`verbosity_level`
     del local_vars['verbosity']
     del local_vars['silence']
@@ -109,7 +130,7 @@ def cli(
         # continues to work as expected
         local_vars['ctx'] = ctx
 
-    main()
+    main(**local_vars)
 
 if __name__ == '__main__':
     cli()
