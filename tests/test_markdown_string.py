e2bd072997c60aca41f5adf78c3682fb8a9632a4
Christian Chavez <christian.chavez@nav.no>
2021-02-24 11:21:55 +0100

    Refactor(tests): Fix tests - update.

--- tests/test_markdown_string.py
+++ tests/test_markdown_string.py
@@ -9,8 +9,8 @@ from hypothesis import given
 from hypothesis import strategies as st
 
 # Imports of module(s) internal to this project/package
-from rosahelikopter.main import repo_filter
-from rosahelikopter.markdown import make_table
+from rosahelikopter import repository_is_relevant_for_overview
+from rosahelikopter.markdown import make_markdown_table
 
 
 MARKDOWN_VALID_TABLE_ROWS_REGEX_PATTERN = r'\| \[[-\w\d]+/[-\w\d]+\]\(.*\) \| .+ \|'
@@ -39,13 +39,7 @@ class hashabledict(dict):
                 nameWithOwner=st.from_regex(GITHUB_NAME_WITH_OWNER, fullmatch=True),
                 url=st.text(alphabet=HYPOTHESIS_GITHUB_NAMES_CHARACTERS, min_size=1),
                 isArchived=st.booleans(),
-                permissions=st.dictionaries(
-                    st.sampled_from(('ADMIN', 'READER', 'USER')),
-                    st.tuples(st.text(),),
-                    dict_class=hashabledict,
-                    min_size=1,
-                    max_size=1,
-                ),
+                permissions=st.sampled_from(('ADMIN', 'READER', 'USER')),
             )
         ),
         unique=True,
@@ -53,9 +47,11 @@ class hashabledict(dict):
     )
 )
 def ensure_markdown_output_table_matches_expected_size(input_repoes: list[dict[str, Union[str, bool]]]) -> bool:
-    repoes = {repo['nameWithOwner']: repo for repo in input_repoes}
-    num_valid_repoes = len([repo for repo in repoes.values() if repo_filter(repo)])
-    num_valid_table_rows = 0
-    for _ in re.finditer(MARKDOWN_VALID_TABLE_ROWS_REGEX_PATTERN, make_table(repoes)):
+    num_valid_table_rows, num_valid_repoes = 0, len([
+        repo
+        for repo in input_repoes
+        if repository_is_relevant_for_overview(repo, repo['permissions'])
+    ])
+    for _ in re.finditer(MARKDOWN_VALID_TABLE_ROWS_REGEX_PATTERN, make_markdown_table(input_repoes)):
         num_valid_table_rows += 1
     assert(num_valid_repoes == num_valid_table_rows)
